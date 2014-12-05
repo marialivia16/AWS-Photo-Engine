a8c81c32610e3c864cc1d472e13b86cbdc13dccc
marialivia16 <maria-livia.chiorean@my.fmi.unibuc.ro>
2014-12-05 10:57:00 +0200

    Add FileUpload to HomePage

--- PhotoEngine/PhotoEngine/Default.aspx
+++ PhotoEngine/PhotoEngine/Default.aspx
@@ -7,7 +7,18 @@
     </div>
 
     <div class="row">
-        
+        <div class="col-sm-12 contentUpload">
+            <asp:FileUpload ID="fileUploader" runat="server" />
+            <asp:RegularExpressionValidator ID="photoValidator" runat="server" ControlToValidate="fileUploader"
+                ErrorMessage="Only images allowed! (jpeg, png)"
+                ValidationExpression="^(([a-zA-Z]:)|(\\{2}\w+)\$?)(\\(\w[\w].*))+(.jpeg|.jpg|.png)$" Display="Dynamic"></asp:RegularExpressionValidator><br/>
+            <asp:Button ID="btnPreview" runat="server" Text="Preview" OnClick="btnPreview_OnClick" CssClass="btn btn-block btn-default" /> <br/>
+            <asp:Panel ID="panelPreview" runat="server" Visible="false">
+                <asp:Image ID="imgPreview" runat="server" />
+                <asp:Button ID="btnSave" runat="server" Text="Upload" OnClick="btnSave_OnClick" CssClass="btn btn-primary" />
+                <asp:Button ID="btnCancel" runat="server" Text="Cancel" OnClick="btnCancel_OnClick" CssClass="btn btn-warning" />
+            </asp:Panel>
+        </div>
     </div>
 
 </asp:Content>
--- PhotoEngine/PhotoEngine/Default.aspx.cs
+++ PhotoEngine/PhotoEngine/Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +13,29 @@ namespace PhotoEngine
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnPreview_OnClick(object sender, EventArgs e)
+        {
+            panelPreview.Visible = false;
+            Session["Image"] = fileUploader.PostedFile;
+            var fs = fileUploader.PostedFile.InputStream;
+            var br = new BinaryReader(fs);
+            var bytes = br.ReadBytes((Int32)fs.Length);
+            var base64String = Convert.ToBase64String(bytes, 0, bytes.Length);
+            imgPreview.ImageUrl = "data:image/png;base64," + base64String;
+            imgPreview.Height = 200;
+            panelPreview.Visible = true;
+        }
+
+        protected void btnSave_OnClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void btnCancel_OnClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
